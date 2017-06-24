8d8c0fbc8344513b0702d7dd64a1d183e12713e5
Sean Tay <sean_tay1999@hotmail.com>
2017-06-24 11:10:00 +0800

    Made some UI for upload page
    It's ugly, I'm still trying to get used to Visual Studio

---
+++ Uploading Page/Uploading/Upload/Page1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Layout.Upload
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void uploadButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("c:\\");
+        }
+    }
+}
