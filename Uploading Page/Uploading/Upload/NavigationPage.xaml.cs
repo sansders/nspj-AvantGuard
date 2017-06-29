f70e03b4930505450df2a842cc1b649f00da2e05
Bryan lee <leeweihan98@yahoo.com.sg>
2017-06-29 22:22:05 +0800

    Established Connectivity with Database (private network)
    :)

--- Uploading Page/Uploading/Upload/NavigationPage.xaml.cs
+++ Uploading Page/Uploading/Upload/NavigationPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +35,33 @@ namespace Layout.Upload
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Page2());
+        }
+
+        private void BtnDB(object sender, RoutedEventArgs e)
+        {
+            //SqlConnection conn = new SqlConnection("Server=WIN-P4IU8GMGT91\\SQLEXPRESS;Database=User;Integrated Security=True");
+            //SqlConnection conn = new SqlConnection("Server=192.168.205.138,1433\\SQLEXPRESS;Network Library=DBMSSOCN;Initial Catalog = User; User ID = root; Password = ; ");
+            SqlConnection conn = new SqlConnection("Data Source=192.168.205.138,1433;Network Library=DBMSSOCN;Initial Catalog = User; User ID = root; Password = 123;Trusted_Connection = no;Integrated Security=False; ");
+            //WHY CANNOT CONNECT
+
+            conn.Open();
+            Console.Write(conn.State);
+           SqlCommand cmd = new SqlCommand("SELECT Username , Name , ContactNo FROM [dbo].[UserAcc]" , conn);
+           // SqlCommand cmd = new SqlCommand("SELECT * FROM UserAcc" , conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            //Console.WriteLine(reader.GetString(0));
+            while (reader.Read()) {
+                
+                Console.WriteLine(" | Username : " + reader.GetString(0) + " | Name : " + reader.GetString(1) + " | Contact No : " + reader.GetString(2));
+            }
+            reader.Close();
+            conn.Close();
+
+          /*  if (Debugger.IsAttached)
+            {
+                Console.ReadLine();
+            } */
+
         }
     }
 }
