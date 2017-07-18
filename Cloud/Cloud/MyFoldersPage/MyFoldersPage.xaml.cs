31b12865227d2d973a7b50897b458279be1bcb38
cosmooo <chester.chia@gmail.com>
2017-07-18 11:48:29 +0800

    big changes to frame and stuff

--- Cloud/Cloud/MyFoldersPage/MyFoldersPage.xaml.cs
+++ Cloud/Cloud/MyFoldersPage/MyFoldersPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,43 +24,52 @@ namespace Cloud.MyFoldersPage
     /// </summary>
     public partial class MyFoldersPage : Page
     {
-        private List<MyItem> newList = new List<MyItem>();
+        private List<String> newList = new List<String>();
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Chester\Documents\test_db.mdf;Integrated Security=True;Connect Timeout=30");
 
         public MyFoldersPage()
         {
             InitializeComponent();
 
-            listView.Items.Add(new MyItem { Name = "Business Presentation", Owner = "WeiHan9898", LastModified = "27 May 2017", FileSize = "903MB" });
-            listView.Items.Add(new MyItem { Name = "My Story", Owner = "SohJT", LastModified = "23 May 2017", FileSize = "742MB" });
-            listView.Items.Add(new MyItem { Name = "MatthewHo.docx", Owner = "Grymb3l", LastModified = "9 May 2017", FileSize = "103MB" });
-            listView.Items.Add(new MyItem { Name = "Chengdu PPT", Owner = "ShonTei", LastModified = "19 April 2017", FileSize = "1.4GB" });
-            listView.Items.Add(new MyItem { Name = "My Presentation", Owner = "me", LastModified = "4 April 2017", FileSize = "78KB" });
-            
-            foreach (MyItem item in listView.Items)
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select docName from [table]";
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            listView.ItemsSource = dt.DefaultView;
+            con.Close();
+
+            foreach (DataRowView item in listView.Items)
             {
-                newList.Add(item);
+                newList.Add(item.ToString());
             }
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-                    
+
             if (!string.IsNullOrWhiteSpace(searchBar.Text))
             {
-                
+
                 listView.Items.Clear();
 
                 for (int i = 0; i < newList.Count; i++)
                 {
                     var item = newList[i];
-                    if (item.Name.ToLower().Contains(searchBar.Text.ToLower()))
+                    if (item.ToLower().Contains(searchBar.Text.ToLower()))
                     {
                         listView.Items.Add(item);
                     }
                 }
 
             }
-            
+
 
             else
             {
@@ -77,5 +90,163 @@ namespace Cloud.MyFoldersPage
             tb.GotFocus -= TextBox_GotFocus;
         }
 
+        private void listView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            String theText = "";
+            String selectedText = ((DataRowView)((ListView)sender).SelectedItem)["docName"].ToString();
+            fileName.Content = selectedText;
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select text from [table] where docName = '" + selectedText + "'";
+            using (SqlDataReader read = cmd.ExecuteReader())
+            {
+                while (read.Read())
+                {
+                    theText = (read["text"].ToString());
+                }
+            }
+            con.Close();
+
+            byte[] byteArray = Encoding.ASCII.GetBytes(theText);
+            using (MemoryStream ms = new MemoryStream(byteArray))
+            {
+                TextRange tr = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                tr.Load(ms, DataFormats.Rtf);
+            }
+
+            listView.Visibility = System.Windows.Visibility.Hidden;
+            mainToolBar.Visibility = System.Windows.Visibility.Hidden;
+            rtbEditor.Visibility = System.Windows.Visibility.Visible;
+            secondToolBar.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void newTextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NewFile.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewFile.Visibility = System.Windows.Visibility.Collapsed;
+
+            String input = textbox1.Text;
+            fileName.Content = input;
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into [table] values('" + textbox1.Text + "', '')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            textbox1.Text = String.Empty;
+
+            listView.Visibility = System.Windows.Visibility.Hidden;
+            mainToolBar.Visibility = System.Windows.Visibility.Hidden;
+            rtbEditor.Visibility = System.Windows.Visibility.Visible;
+            secondToolBar.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewFile.Visibility = System.Windows.Visibility.Collapsed;
+            textbox1.Text = String.Empty;
+        }
+
+        private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbFontFamily.SelectedItem != null)
+                rtbEditor.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+        }
+
+        private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+        }
+
+        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            cmbFontFamily.SelectedItem = temp;
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            cmbFontSize.Text = temp.ToString();
+        }
+
+        private void colorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            rtbEditor.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, (SolidColorBrush)(new BrushConverter().ConvertFrom(colorPicker.SelectedColor.ToString())));
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            String rtfText;
+            TextRange tr = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                tr.Save(ms, DataFormats.Rtf);
+                rtfText = Encoding.ASCII.GetString(ms.ToArray());
+            }
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update [table] set text = '" + rtfText + "' where docName = '" + fileName.Content + "'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Save was done.");
+        }
+
+        public void openMSBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.DefaultExt = ".doc";
+            openFileDialog.Filter = "Word Documents(*.doc;*.docx)|*.doc;*.docx";
+
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                String filename = System.IO.Path.GetFileNameWithoutExtension(openFileDialog.FileName);
+                String fullfilename = System.IO.Path.GetFullPath(openFileDialog.FileName);
+                Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+
+                object File = fullfilename;
+                object nullobject = System.Reflection.Missing.Value;
+
+                Microsoft.Office.Interop.Word.Application wordApp2 = new Microsoft.Office.Interop.Word.Application();
+                wordApp2.DisplayAlerts = Microsoft.Office.Interop.Word.WdAlertLevel.wdAlertsNone;
+                Microsoft.Office.Interop.Word.Document docs = wordApp.Documents.Open(ref File, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject, ref nullobject);
+                docs.ActiveWindow.Selection.WholeStory();
+                docs.ActiveWindow.Selection.Copy();
+                docs.Close();
+                wordApp.Quit();
+
+                fileName.Content = filename;
+                rtbEditor.Document.Blocks.Clear();
+                rtbEditor.Paste();
+
+                String rtfText;
+                TextRange tr = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    tr.Save(ms, DataFormats.Rtf);
+                    rtfText = Encoding.ASCII.GetString(ms.ToArray());
+                }
+
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "insert into [table] values('" + fileName.Content + "', '" + rtfText + "')";
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
     }
 }
